cfe211a4b3641366ca4712014326a7dd11db5e29
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-02-25 20:35:03 +0100

    ad

--- app/src/components/widget/Widget.jsx
+++ app/src/components/widget/Widget.jsx
@@ -1,24 +1,93 @@
-import "./widget.scss"
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import PersonIcon from '@mui/icons-material/Person';
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonIcon from "@mui/icons-material/Person";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 
-const Widget = () => {
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary
+
+  const amount = 100;
+  const diff = 30;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "#f06745b9" }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "#6ceb6cb8" }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnIcon
+            className="icon"
+            style={{ color: "gold", backgroundColor: "#f8f666" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletIcon
+            className="icon"
+            style={{ color: "blue", backgroundColor: "#4b71f0" }}
+          />
+        ),
+      };
+    default:
+      break;
+  }
+
+  console.log(data);
   return (
     <div className="widget">
       <div className="left">
-        <span className="title">USERS</span>
-        <span className="counter">21321</span>
-        <span className="link">See all user</span>
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
       </div>
       <div className="right">
         <div className="percentage negative">
-            <KeyboardArrowUpIcon/>
-            20%
+          <KeyboardArrowUpIcon />
+          {diff} %
         </div>
-        <PersonIcon className="icon"/>
+        {data.icon}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Widget
+export default Widget;
